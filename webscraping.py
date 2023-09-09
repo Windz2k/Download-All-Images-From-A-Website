ab1ac5887c812fa1fbdd3b83c1310e3011b68f0d
Charles <cdewind@outlook.com>
2023-09-09 14:20:37 +0100

    Updated the image searcher
    Signed-off-by: Charles <cdewind@outlook.com>

--- webscraping.py
+++ webscraping.py
@@ -15,16 +15,21 @@ class WebScraper:
             r.raise_for_status()
             soup = bs4.BeautifulSoup(r.text, "html.parser")
             self.__websites[websiteName] = soup
-            self.get_images(websiteName)
+            self.get_images(websiteName, website)
 
     def get_websites(self):
         return self.__websites
 
     # Will Run Through img tags and download them
-    def get_images(self, websiteName):
+    def get_images(self, websiteName, website):
         # rename this to be the folder where all the images are saved
         location = "myimages"
+        
+        if not os.path.isdir(location):
+            os.mkdir(os.getcwd() + "/" + location)
+        
         os.chdir(location)
+        
         try:
             os.mkdir(os.getcwd() +"/" + websiteName.strip())
         except Exception:
@@ -33,43 +38,47 @@ class WebScraper:
         
         soupObj = self.__websites[websiteName]
         all_images = soupObj.find_all("img")
-        links = list(map(lambda x :x["src"], all_images))
+        
+        links = list(map(lambda x :x.attrs["src"] if "src" in x.attrs else None, all_images))
 
         # Runs Through All The Images on The Website
         # And Attempts to Download Images Will Not Work with AWS Images
         for link in links:
-            with open("image%s.jpg" % (self.__image_counter), mode="wb") as file:
-                try:
-                    response = requests.get(link)
-                    response.raise_for_status()
-                    for x in response.iter_content(100000):
-                        file.write(x)
-                except IOError:
-                    print(link + "\nRestricts us from downloading the image")
-                    continue
-                except Exception:
-                    print("There was an error with this file")
-                    continue
+            
+            if link is not None:
+            
+                with open("image%s.jpg" % (self.__image_counter), mode="wb") as file:
+                    try:
+                        response = None
+                        if "https:" in link:
+                            response = requests.get(link)
+                            response.raise_for_status()
+                        else:
+                            response = requests.get(website+link)
+                            response.raise_for_status()
+                        for x in response.iter_content(100000):
+                            file.write(x)
+                    except IOError:
+                        print(link + "\nRestricts us from downloading the image")
+                        continue
+                    except Exception:
+                        print("There was an error with this file")
+                        continue
 
-            self.__image_counter += 1
+                self.__image_counter += 1
 
 
 my_user_agent = ""
 
 w = WebScraper(my_user_agent)
 
-answer = input("Enter Y If You Would Like To Websrape a Website:")
-while answer == "Y" or answer=="y":
-    print("You Will Be Prompted To Enter The Name Of The Website and The URL Of The Website!")    
-    name_of_website = input("Enter the name of the website:")
-    url_of_website = input("Enter the url of the website:")
+answer = input("Enter Yes If You Would Like To Websrape a Website: ")
+while answer.upper() == "YES":   
+    name_of_website = input("Enter the name of the website: ")
+    url_of_website = input("Enter the url of the website: ")
     w(url_of_website, name_of_website)
-    answer = input("Enter Y If You Would Like To Websrape a Website:")
+    print("\n")
+    answer = input("Enter Y If You Would Like To Websrape a Website: ")
     
-print("Thanks For Using My Service :)")
+print("Thanks For Using My Service :) \n")
 sys.exit()
-
-# w("https://www.bbc.com/","BBC")
-# # print(w.get_websites()["BBC"])
-# w("https://www.herschel.slough.sch.uk/", "Herschel")
-# w("https://www.youtube.com/", "Youtube")
